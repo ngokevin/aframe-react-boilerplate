bfc222648954769f5122d942065f2864c2e00308
Kevin Ngo <me@ngokevin.com>
2015-12-10 16:08:52 -0500

    show off cursor and events

--- src/js/app.js
+++ src/js/app.js
@@ -5,14 +5,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Camera from './components/Camera';
+import Cursor from './components/Cursor';
 import Light from './components/Light';
 import Sky from './components/Sky';
 
 class BoilerplateScene extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'red'
+    }
+  }
+
+  changeColor = () => {
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
+    this.setState({
+      color: colors[Math.floor(Math.random() * colors.length)],
+    });
+  }
+
   render () {
     return (
       <Scene>
-        <Camera/>
+        <Camera><Cursor/></Camera>
 
         <Sky/>
 
@@ -20,7 +35,9 @@ class BoilerplateScene extends React.Component {
         <Light type="directional" intensity="0.5" position="-1 1 0"/>
         <Light type="directional" intensity="1" position="1 1 0"/>
 
-        <Entity geometry={{primitive: 'box'}} material="color: red" position="0 0 -5">
+        <Entity geometry="primitive: box" material={{color: this.state.color}}
+                onClick={this.changeColor}
+                position="0 0 -5">
           <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
         </Entity>
       </Scene>
