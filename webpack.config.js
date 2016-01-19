1359d92dea6dcd20878f77d740d1bb85064f6b18
caseyyee <caseyyee.ca@gmail.com>
2016-01-18 18:38:07 -0800

    use aframe instead of aframe-core

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,10 @@ module.exports = {
       {
         test: /\.json$/,
         loader: 'json-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
       }
     ],
   },
