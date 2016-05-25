8853be2b767b6bbd6f8c74ab16aeb7b2f7dc7090
Kevin Ngo <me@ngokevin.com>
2016-05-24 18:45:15 -0700

    reduce sky radius

--- src/js/components/Sky.js
+++ src/js/components/Sky.js
@@ -2,7 +2,7 @@ import {Entity} from 'aframe-react';
 import React from 'react';
 
 export default props => (
-  <Entity geometry={{primitive: 'sphere', radius: 5000}}
+  <Entity geometry={{primitive: 'sphere', radius: 100}}
           material={{color: "#73CFF0", shader: 'flat'}}
           scale="1 1 -1"/>
 );
