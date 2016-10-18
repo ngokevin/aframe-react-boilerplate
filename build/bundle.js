5337039b02f5cec3e0d29230ea1ac87224588f97
Kevin Ngo <me@ngokevin.com>
2016-10-18 13:10:18 -0700

    bump bundle

--- build/bundle.js
+++ build/bundle.js
@@ -73,15 +73,15 @@
 
 	var _reactDom2 = _interopRequireDefault(_reactDom);
 
-	var _Camera = __webpack_require__(371);
+	var _Camera = __webpack_require__(372);
 
 	var _Camera2 = _interopRequireDefault(_Camera);
 
-	var _Text = __webpack_require__(372);
+	var _Text = __webpack_require__(373);
 
 	var _Text2 = _interopRequireDefault(_Text);
 
-	var _Sky = __webpack_require__(373);
+	var _Sky = __webpack_require__(374);
 
 	var _Sky2 = _interopRequireDefault(_Sky);
 
@@ -121,7 +121,9 @@
 	        _react2.default.createElement(
 	          _Camera2.default,
 	          null,
-	          _react2.default.createElement('a-cursor', null)
+	          _react2.default.createElement('a-cursor', {
+	            animation__click: 'property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150'
+	          })
 	        ),
 	        _react2.default.createElement(_Sky2.default, { src: 'url(https://rawgit.com/aframevr/assets/gh-pages/360-image-gallery-boilerplate/img/sechelt.jpg)' }),
 	        _react2.default.createElement(_Text2.default, {
@@ -141,7 +143,7 @@
 	            position: '0 -0.5 -3',
 	            onClick: this.changeColor },
 	          _react2.default.createElement(_aframeReact.Entity, {
-	            animation__sca: { property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2' },
+	            animation__scale: { property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2' },
 	            geometry: 'primitive: box; depth: 0.2; height: 0.2; width: 0.2',
 	            material: { color: '#24CAFF' } })
 	        )
@@ -74411,7 +74413,7 @@
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
-	exports.Scene = exports.Entity = exports.Animation = undefined;
+	exports.Scene = exports.Entity = undefined;
 	exports.serializeComponents = serializeComponents;
 
 	var _react = __webpack_require__(200);
@@ -74426,6 +74428,8 @@
 
 	var _styleAttr2 = _interopRequireDefault(_styleAttr);
 
+	var _eventUtils = __webpack_require__(371);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -74435,16 +74439,131 @@
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 	/**
-	 * Stringify components passed as an object.
+	 * <a-entity>
+	 */
+
+	var Entity = exports.Entity = (function (_React$Component) {
+	  _inherits(Entity, _React$Component);
+
+	  function Entity() {
+	    var _Object$getPrototypeO;
+
+	    var _temp, _this, _ret;
+
+	    _classCallCheck(this, Entity);
+
+	    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+	      args[_key] = arguments[_key];
+	    }
+
+	    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(Entity)).call.apply(_Object$getPrototypeO, [this].concat(args))), _this), _this.attachEvents = function (el) {
+	      if (!el) {
+	        return;
+	      }
+	      attachEventsToElement(el, Object.assign({}, _this.props.events, (0, _eventUtils.getEventMappings)(_this.props)));
+	    }, _temp), _possibleConstructorReturn(_this, _ret);
+	  }
+
+	  _createClass(Entity, [{
+	    key: 'render',
+	    value: function render() {
+	      var _this2 = this;
+
+	      // Allow through normal attributes..
+	      var otherProps = {};
+	      ['className', 'id', 'mixin'].forEach(function (propName) {
+	        if (_this2.props[propName]) {
+	          otherProps[propName] = _this2.props[propName];
+	        }
+	      });
+
+	      return _react2.default.createElement(
+	        'a-entity',
+	        _extends({
+	          ref: this.attachEvents
+	        }, otherProps, serializeComponents(this.props)),
+	        this.props.children
+	      );
+	    }
+	  }]);
+
+	  return Entity;
+	})(_react2.default.Component);
+
+	/**
+	 * <a-scene>
+	 */
+
+	Entity.propTypes = {
+	  children: _react2.default.PropTypes.any,
+	  events: _react2.default.PropTypes.object,
+	  mixin: _react2.default.PropTypes.string
+	};
+
+	var Scene = exports.Scene = (function (_React$Component2) {
+	  _inherits(Scene, _React$Component2);
+
+	  function Scene() {
+	    var _Object$getPrototypeO2;
+
+	    var _temp2, _this3, _ret2;
+
+	    _classCallCheck(this, Scene);
+
+	    for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+	      args[_key2] = arguments[_key2];
+	    }
+
+	    return _ret2 = (_temp2 = (_this3 = _possibleConstructorReturn(this, (_Object$getPrototypeO2 = Object.getPrototypeOf(Scene)).call.apply(_Object$getPrototypeO2, [this].concat(args))), _this3), _this3.attachEvents = function (el) {
+	      if (!el) {
+	        return;
+	      }
+	      attachEventsToElement(el, Object.assign({}, _this3.props.events, (0, _eventUtils.getEventMappings)(_this3.props)));
+	    }, _temp2), _possibleConstructorReturn(_this3, _ret2);
+	  }
+
+	  _createClass(Scene, [{
+	    key: 'render',
+	    value: function render() {
+	      var _this4 = this;
+
+	      // Allow through normal attributes..
+	      var otherProps = {};
+	      ['className', 'id', 'mixin'].forEach(function (propName) {
+	        if (_this4.props[propName]) {
+	          otherProps[propName] = _this4.props[propName];
+	        }
+	      });
+
+	      return _react2.default.createElement(
+	        'a-scene',
+	        _extends({
+	          ref: this.attachEvents
+	        }, otherProps, serializeComponents(this.props)),
+	        this.props.children
+	      );
+	    }
+	  }]);
+
+	  return Scene;
+	})(_react2.default.Component);
+
+	/**
+	 * Serialize React props to A-Frame components.
 	 *
 	 * {primitive: box; width: 10} to 'primitive: box; width: 10'
 	 */
+
+	Scene.propTypes = {
+	  events: _react2.default.PropTypes.object
+	};
 	function serializeComponents(props) {
 	  var components = AFRAME.components;
 
 	  var serialProps = {};
 	  Object.keys(props).forEach(function (component) {
-	    if (['children', 'mixin'].indexOf(component) !== -1) {
+	    // Allow these.
+	    if (['class', 'children', 'className', 'id', 'mixin'].indexOf(component) !== -1) {
 	      return;
 	    }
 
@@ -74459,7 +74578,7 @@
 	    }
 
 	    if (props[component].constructor === Array) {
-	      //Stringify components passed as array.
+	      // Stringify components passed as array.
 	      serialProps[component] = props[component].join(' ');
 	    } else if (props[component].constructor === Object) {
 	      // Stringify components passed as object.
@@ -74484,208 +74603,19 @@
 	  return serialProps;
 	};
 
-	var Animation = exports.Animation = (function (_React$Component) {
-	  _inherits(Animation, _React$Component);
-
-	  function Animation() {
-	    var _Object$getPrototypeO;
-
-	    var _temp, _this, _ret;
-
-	    _classCallCheck(this, Animation);
-
-	    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-	      args[_key] = arguments[_key];
-	    }
-
-	    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_Object$getPrototypeO = Object.getPrototypeOf(Animation)).call.apply(_Object$getPrototypeO, [this].concat(args))), _this), _this.attachEvents = function (el) {
-	      if (el) {
-	        el.addEventListener('animationend', function (event) {
-	          _this.props.onAnimationEnd(event);
-	        });
-	        el.addEventListener('animationstart', function (event) {
-	          _this.props.onAnimationStart(event);
-	        });
-	      }
-	    }, _temp), _possibleConstructorReturn(_this, _ret);
+	/**
+	 * Register event handlers to ref.
+	 */
+	function attachEventsToElement(el, eventMap) {
+	  if (!eventMap) {
+	    return;
 	  }
-
-	  _createClass(Animation, [{
-	    key: 'render',
-	    value: function render() {
-	      return _react2.default.createElement('a-animation', _extends({ ref: this.attachEvents }, serializeComponents(this.props)));
-	    }
-	  }]);
-
-	  return Animation;
-	})(_react2.default.Component);
-
-	Animation.propTypes = {
-	  onAnimationEnd: _react2.default.PropTypes.func,
-	  onAnimationStart: _react2.default.PropTypes.func
-	};
-	Animation.defaultProps = {
-	  onAnimationEnd: function onAnimationEnd() {},
-	  onAnimationStart: function onAnimationStart() {}
-	};
-
-	var Entity = exports.Entity = (function (_React$Component2) {
-	  _inherits(Entity, _React$Component2);
-
-	  function Entity() {
-	    var _Object$getPrototypeO2;
-
-	    var _temp2, _this2, _ret2;
-
-	    _classCallCheck(this, Entity);
-
-	    for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
-	      args[_key2] = arguments[_key2];
-	    }
-
-	    return _ret2 = (_temp2 = (_this2 = _possibleConstructorReturn(this, (_Object$getPrototypeO2 = Object.getPrototypeOf(Entity)).call.apply(_Object$getPrototypeO2, [this].concat(args))), _this2), _this2.attachEvents = function (el) {
-	      if (el) {
-	        el.addEventListener('cursor-click', function (event) {
-	          _this2.props.onClick(event);
-	        });
-	        el.addEventListener('loaded', function (event) {
-	          _this2.props.onLoaded(event);
-	        });
-	        el.addEventListener('cursor-mouseenter', function (event) {
-	          _this2.props.onMouseEnter(event);
-	        });
-	        el.addEventListener('cursor-mouseleave', function (event) {
-	          _this2.props.onMouseLeave(event);
-	        });
-	        el.addEventListener('cursor-mousedown', function (event) {
-	          _this2.props.onMouseDown(event);
-	        });
-	        el.addEventListener('cursor-mouseup', function (event) {
-	          _this2.props.onMouseUp(event);
-	        });
-	        el.addEventListener('child-attached', function (event) {
-	          _this2.props.onChildAttached(event);
-	        });
-	        el.addEventListener('componentchanged', function (event) {
-	          _this2.props.onComponentChanged(event);
-	        });
-	        el.addEventListener('pause', function (event) {
-	          _this2.props.onPause(event);
-	        });
-	        el.addEventListener('play', function (event) {
-	          _this2.props.onPlay(event);
-	        });
-	        el.addEventListener('stateadded', function (event) {
-	          _this2.props.onStateAdded(event);
-	        });
-	        el.addEventListener('stateremoved', function (event) {
-	          _this2.props.onStateRemoved(event);
-	        });
-	      }
-	    }, _temp2), _possibleConstructorReturn(_this2, _ret2);
-	  }
-
-	  _createClass(Entity, [{
-	    key: 'render',
-	    value: function render() {
-	      var mixinProp = this.props.mixin ? { mixin: this.props.mixin } : {};
-
-	      return _react2.default.createElement(
-	        'a-entity',
-	        _extends({ ref: this.attachEvents
-	        }, mixinProp, serializeComponents(this.props)),
-	        this.props.children
-	      );
-	    }
-	  }]);
-
-	  return Entity;
-	})(_react2.default.Component);
-
-	Entity.propTypes = {
-	  children: _react2.default.PropTypes.any,
-	  mixin: _react2.default.PropTypes.string,
-	  onClick: _react2.default.PropTypes.func,
-	  onLoaded: _react2.default.PropTypes.func,
-	  onMouseEnter: _react2.default.PropTypes.func,
-	  onMouseLeave: _react2.default.PropTypes.func,
-	  onMouseDown: _react2.default.PropTypes.func,
-	  onMouseUp: _react2.default.PropTypes.func,
-	  onChildAttached: _react2.default.PropTypes.func,
-	  onComponentChanged: _react2.default.PropTypes.func,
-	  onPause: _react2.default.PropTypes.func,
-	  onPlay: _react2.default.PropTypes.func,
-	  onStateAdded: _react2.default.PropTypes.func,
-	  onStateRemoved: _react2.default.PropTypes.func
-	};
-	Entity.defaultProps = {
-	  onClick: function onClick() {},
-	  onLoaded: function onLoaded() {},
-	  onMouseEnter: function onMouseEnter() {},
-	  onMouseLeave: function onMouseLeave() {},
-	  onMouseDown: function onMouseDown() {},
-	  onMouseUp: function onMouseUp() {},
-	  onChildAttached: function onChildAttached() {},
-	  onComponentChanged: function onComponentChanged() {},
-	  onPause: function onPause() {},
-	  onPlay: function onPlay() {},
-	  onStateAdded: function onStateAdded() {},
-	  onStateRemoved: function onStateRemoved() {}
-	};
-
-	var Scene = exports.Scene = (function (_React$Component3) {
-	  _inherits(Scene, _React$Component3);
-
-	  function Scene() {
-	    var _Object$getPrototypeO3;
-
-	    var _temp3, _this3, _ret3;
-
-	    _classCallCheck(this, Scene);
-
-	    for (var _len3 = arguments.length, args = Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
-	      args[_key3] = arguments[_key3];
-	    }
-
-	    return _ret3 = (_temp3 = (_this3 = _possibleConstructorReturn(this, (_Object$getPrototypeO3 = Object.getPrototypeOf(Scene)).call.apply(_Object$getPrototypeO3, [this].concat(args))), _this3), _this3.attachEvents = function (el) {
-	      if (el) {
-	        el.addEventListener('enter-vr', function (event) {
-	          _this3.props.onEnterVR(event);
-	        });
-	        el.addEventListener('exit-vr', function (event) {
-	          _this3.props.onExitVR(event);
-	        });
-	        el.addEventListener('loaded', function (event) {
-	          _this3.props.onLoaded(event);
-	        });
-	      }
-	    }, _temp3), _possibleConstructorReturn(_this3, _ret3);
-	  }
-
-	  _createClass(Scene, [{
-	    key: 'render',
-	    value: function render() {
-	      return _react2.default.createElement(
-	        'a-scene',
-	        _extends({ ref: this.attachEvents }, serializeComponents(this.props)),
-	        this.props.children
-	      );
-	    }
-	  }]);
-
-	  return Scene;
-	})(_react2.default.Component);
-
-	Scene.propTypes = {
-	  onEnterVR: _react2.default.PropTypes.func,
-	  onExitVR: _react2.default.PropTypes.func,
-	  onLoaded: _react2.default.PropTypes.func
-	};
-	Scene.defaultProps = {
-	  onEnterVR: function onEnterVR() {},
-	  onExitVR: function onExitVR() {},
-	  onLoaded: function onLoaded() {}
-	};
+	  Object.keys(eventMap).forEach(function (eventName) {
+	    el.addEventListener(eventName, function (event) {
+	      eventMap[eventName](event);
+	    });
+	  });
+	}
 
 /***/ },
 /* 200 */
@@ -95982,6 +95912,39 @@
 
 /***/ },
 /* 371 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	exports.getEventMappings = getEventMappings;
+	function titleToKebabCase(str) {
+	  return str.replace(/([^-])([A-Z])/, '$1-$2').toLowerCase();
+	}
+
+	/**
+	 * Determines event listeners to handle using props that start with `on`.
+	 * For example, `onClick` would prescribe to register an event handler for
+	 * `click`. `onIntersectionCleared` would prescribe to register event handler
+	 * for `intersection-cleared`.
+	 *
+	 * @param {object} possibleEventHandlers - React props object.
+	 * @returns {object} Events to register.
+	 */
+	function getEventMappings(possibleEventHandlers) {
+	  return Object.keys(possibleEventHandlers).filter(function (reactEventName) {
+	    return !!reactEventName.match(/^on[A-Z]/) && possibleEventHandlers[reactEventName].constructor === Function;
+	  }).reduce(function (handlers, reactEventName) {
+	    var aframeEventName = titleToKebabCase(reactEventName.replace(/^on/, ''));
+	    handlers[aframeEventName] = possibleEventHandlers[reactEventName];
+	    return handlers;
+	  }, {});
+	}
+
+/***/ },
+/* 372 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -96009,7 +95972,7 @@
 	};
 
 /***/ },
-/* 372 */
+/* 373 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -96039,7 +96002,7 @@
 	};
 
 /***/ },
-/* 373 */
+/* 374 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
