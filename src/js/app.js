76bb047b7b2c746fbad04aa1b3089b05dabac0bb
John Winslow <winslow.johnm@gmail.com>
2016-12-19 10:59:10 -0800

    Add vrMode button, mess with VR display

--- src/js/app.js
+++ src/js/app.js
@@ -13,8 +13,14 @@ import Sky from './components/Sky';
 class VRScene extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {color: 'red'};
+    this.state = {color: 'red', vrMode: false};
+    this.changeVRMode = this.changeVRMode.bind(this)
   }
+
+  changeVRMode() {
+      console.log("hello")
+      this.setState({vrMode: true})
+    }
 
   changeColor() {
     const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
@@ -24,40 +30,26 @@ class VRScene extends React.Component {
   }
 
   render () {
-    return (
-      <Scene>
-        <Camera>
-          <a-cursor
-            animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150"
->
-          </a-cursor>
-        </Camera>
+    if (this.state.vrMode) {
 
-        <Sky src="url(https://rawgit.com/aframevr/assets/gh-pages/360-image-gallery-boilerplate/img/sechelt.jpg)"/>
+      return (
+        <Scene>
+          <Camera>
+            <a-cursor
+              animation__click="property: scale; startEvents: click; from: 0.1 0.1 0.1; to: 1 1 1; dur: 150">
+            </a-cursor>
+          </Camera>
 
-        <Text
-          text='Hello World!'
-          color='#DADADA'
-          position='-1.75 1 -3'/>
-
-        <Entity light={{type: 'ambient', color: '#888'}}/>
-        <Entity light={{type: 'directional', intensity: 0.5}} position='-1 1 0'/>
-        <Entity light={{type: 'directional', intensity: 1}} position='1 1 0'/>
-
-        <Entity
-          animation__rot={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
-          animation__sca={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
-          geometry='primitive: box'
-          material={{color: this.state.color, opacity: 0.6}}
-          position='0 -0.5 -3'
-          onClick={this.changeColor.bind(this)}>
-          <Entity
-            animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
-            geometry='primitive: box; depth: 0.2; height: 0.2; width: 0.2'
-            material={{color: '#24CAFF'}}/>
-        </Entity>
-      </Scene>
-    );
+          
+        </Scene>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.changeVRMode}>VR Mode</button>
+        </div>
+      )
+    }
   }
 }
 
