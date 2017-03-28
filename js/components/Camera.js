b2719a422aef6a6feeb37849f85623318731b4f0
Kevin Ngo <me@ngokevin.com>
2017-03-28 03:48:16 -0700

    Updates

--- js/components/Camera.js
+++
@@ -1,8 +0,0 @@
-import {Entity} from 'aframe-react';
-import React from 'react';
-
-export default props => (
-  <Entity>
-    <Entity camera look-controls wasd-controls {...props}/>
-  </Entity>
-);
--- js/components/Sky.js
+++
@@ -1,8 +0,0 @@
-import {Entity} from 'aframe-react';
-import React from 'react';
-
-export default props => (
-  <Entity geometry={{primitive: 'sphere', radius: 5000}}
-          material={{color: "#73CFF0", shader: 'flat'}}
-          scale="1 1 -1"/>
-);
